fd6de4bcc6fb80d20a989afd12dada997996affa
konghiran.w <konghiran.w@linecorp.com>
2020-10-22 17:01:16 +0700

    add multiple line string

---
+++ src/string/how-do-you-write-multiline-strings-in-Go.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// https://stackoverflow.com/questions/7933460/how-do-you-write-multiline-strings-in-go
+func main() {
+	fmt.Println(`
+	line 1
+	line 2
+	line 3
+	`)
+}
